5d792915c4ed39dbbae8f0434331076a53bd95ef
Adrish <2006.adrish@gmail.com>
2026-05-02 12:05:54 +0530

    hmm

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,20 @@ from flask import Flask, request, jsonify
 import sqlite3
 from flask_cors import CORS
 from flask import redirect
-
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+import atexit
 app = Flask(__name__)
 CORS(app)
 DB = "pulse.db"
+
+def clear_posts():
+    conn = sqlite3.connect(DB)
+    c = conn.cursor()
+    c.execute("DELETE FROM posts")
+    conn.commit()
+    conn.close()
+    print("🧹 Echo wiped at", time.ctime())
 
 def init_db():
     conn = sqlite3.connect(DB)
@@ -23,6 +33,12 @@ def init_db():
     conn.close()
 
 init_db()
+scheduler = BackgroundScheduler(daemon=True)
+
+def start_scheduler():
+    scheduler.add_job(clear_posts, "interval", hours=1)
+    scheduler.start()
+    
 
 @app.route("/")
 def home():
@@ -61,13 +77,18 @@ def get_posts():
     conn.close()
     return jsonify(rows)
 
+def shutdown_scheduler():
+    try:
+        scheduler.shutdown()
+        print("🧠 Scheduler safely shut down")
+    except Exception as e:
+        print("Scheduler shutdown skipped:", e)
 
+atexit.register(shutdown_scheduler)
         
 import os
 
 if __name__ == "__main__":
+    start_scheduler()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
-
